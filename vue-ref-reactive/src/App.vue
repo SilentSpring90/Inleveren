b3156ad9c0f8d0a9802f6f7fd0223510ba37f1f2
SilentSpring90 <jefryvputten@gmail.com>
2026-06-04 10:27:16 +0200

    Initial commit

---
+++ vue-ref-reactive/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2>Werken met ref en reactive</h2>
+
+    <p>Aantal clicks: {{ clicks }}</p>
+    <p>Naam: {{ person.firstName }}</p>
+    <p>Land: {{ person.country }}</p>
+
+    <button @click="incrementClicks">Klik mij!</button>
+    <button @click="changeCountry('Nederland')">Nederland</button>
+    <button @click="changeCountry('België')">België</button>
+
+    <input v-model="person.firstName" placeholder="Voornaam" />
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive } from 'vue';
+
+const clicks = ref(0);
+
+const person = reactive({
+  firstName: '',
+  country: 'Nederland'
+});
+
+const incrementClicks = () => {
+  clicks.value++;
+};
+
+const changeCountry = (newCountry) => {
+  person.country = newCountry;
+};
+</script>
