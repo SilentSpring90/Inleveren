7e444d55945ba3b3f801f144fff76eb26488c628
SilentSpring90 <jefryvputten@gmail.com>
2026-06-04 11:22:43 +0200

    rework

---
+++ v-for/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Todo-lijst met v-for</h2>
+
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        {{ task.name }}
+        ({{ task.completed ? 'voltooid' : 'niet voltooid' }})
+
+        <button @click="toggleTask(task)">Toggle</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const tasks = ref([
+  { name: 'Boodschappen doen', completed: false },
+  { name: 'Vaatwasser', completed: true },
+  { name: 'Kat eten geven', completed: false },
+  { name: 'Was doen', completed: false },
+  { name: 'Koken', completed: false },
+  { name: 'Gamen', completed: false },
+  { name: 'Stofzuigen', completed: false },
+])
+
+const toggleTask = (task) => {
+  task.completed = !task.completed
+}
+</script>
